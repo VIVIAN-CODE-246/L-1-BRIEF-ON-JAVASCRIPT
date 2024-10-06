2b84b718e5ae167c7ddf8256bbcd7a326831ff13
VIVIAN-CODE-246 <zaraconcept247@gmail.com>
2024-10-06 13:54:08 +0300

    DATA TYPE AND CONDITIONAL STATEMENT

---
+++ script.js
@@ -0,0 +1,30 @@
+var name="VIVIAN"
+document.write(typeof(name))
+document.write("<br><br>")
+var age= 16
+document.write(typeof(age))
+document.write("<br><br>")
+var is_vehicle=true
+document.write(typeof(true))
+document.write("<br><br>")
+var x;
+document.write(typeof(x))
+document.write("<br><br>")
+var y = null
+document.write(typeof(y))
+document.write("<br><br>")
+var number = [1,2,3]
+document.write(typeof(number))
+document.write("<br>")
+var n = {
+    name:"VIVIAN"
+}
+document.write("<br>")
+document.write(typeof(n))
+document.write("<br><br>")
+if (age < 15){
+    document.write("You are not my mate")
+}
+else {
+    document.write("You are my mate")
+}
